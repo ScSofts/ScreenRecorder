295a1316dde57f0ab4c320612463ef6e3e013064
Root <3416616002@qq.com>
2022-06-26 18:06:22 +0800

    Add okteto configuration file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button, styled} from "@mui/material";
+import {useCallback, useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const PreviewView = styled("video")({
+    width: "100%",
+    height: "auto"
+});
+
+enum RecorderState{
+    Stopped = "Stopped",
+    Recording = "Recording",
+    Paused = "Paused",
+    UnKnown = "UnKnown"
 }
 
-export default App;
+let buttonTextMap = new Map(
+[
+        [RecorderState.Recording, "Stop"],
+        [RecorderState.UnKnown, "Start"],
+        [RecorderState.Stopped, "Start"],
+        [RecorderState.Paused, "Stop"]
+    ]
+);
+
+let videoBuffer:Blob|null = null;
+let stream:MediaStream|null = null;
+let videoRecorder:MediaRecorder|null = null;
+
+
+function App(){
+    const [state,setState] = useState(RecorderState.UnKnown);
+    const [src,setSrc] = useState("");
+
+    const handleClick = useCallback(async()=>{
+        switch (state){
+            case RecorderState.Paused:
+            case RecorderState.Recording:
+                (videoRecorder as MediaRecorder).ondataavailable= (buffer)=> {
+                    videoBuffer = new Blob([buffer.data],{
+                        "type":"video/webm;codecs=h264"
+                    });
+
+                };
+                (videoRecorder as MediaRecorder).onstop = ()=>setSrc(URL.createObjectURL(videoBuffer as Blob));
+                stream?.getTracks().every(value => {
+                    value.stop();
+                    stream?.removeTrack(value);
+                })
+                videoRecorder?.stop();
+                videoRecorder = null;
+                stream = null;
+                console.log(videoBuffer);
+                setState(RecorderState.Stopped);
+                break;
+            case RecorderState.Stopped:
+            case RecorderState.UnKnown:
+                stream = await navigator.mediaDevices.getDisplayMedia({"video":true,"audio":true});
+                videoRecorder = new MediaRecorder(stream,{
+                    mimeType: 'video/webm;codecs=h264',
+                    audioBitsPerSecond : 128000,
+                    videoBitsPerSecond : 250000000
+                })
+                videoRecorder.start();
+                setState(RecorderState.Recording);
+                break;
+        }
+    },[state,setState]);
+
+    let handlePause = ()=>{
+        state === RecorderState.Paused ? setState(RecorderState.Recording) : setState(RecorderState.Paused);
+        videoRecorder?.state === "recording" ? videoRecorder?.pause() : videoRecorder?.resume();
+    };
+
+    return (<>
+        <Button variant={"outlined"} onClick={handleClick}>
+            {buttonTextMap.get(state)}
+        </Button>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <Button variant={"outlined"} onClick={handlePause} disabled={
+                state !== RecorderState.Recording &&
+                state !== RecorderState.Paused
+        }>
+            Pause
+        </Button>
+        <div>
+            <PreviewView controls={true} style={
+                {display:state === RecorderState.Stopped ?"block":"none"}
+            } src={src}/>
+        </div>
+    </>);
+}
+
+export {App};
